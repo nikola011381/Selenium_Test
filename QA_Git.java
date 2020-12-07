9d9919f16aaa7790772ba66cc742a2a7d9b4e891
Nikola <37514211+nikola011381@users.noreply.github.com>
2020-12-07 21:15:41 +0100

    Update QA_Git.java

--- QA_Git.java
+++ QA_Git.java
@@ -11,7 +11,7 @@ public class QA_Git
         driver.manage().window().maximize();
         driver.get("https://github.com/");
         driver.findElement(By.id("user[login]")).sendKeys("s12313134");
-        driver.findElement(By.id("user[email]")).sendKeys("s12313134grgr@gmail.com");
+        driver.findElement(By.id("user[email]")).sendKeys("s12313134grgr@s12313134.com");
         driver.findElement(By.id("user[password]")).sendKeys("s12313134");
         driver.findElement(By.xpath("//*[contains(text(), 'Sign up for GitHub')]")).click();
 
