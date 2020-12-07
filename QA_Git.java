c15763ac129fbe65eb26a5e2fc5f4b0568db5b59
Nikola <37514211+nikola011381@users.noreply.github.com>
2020-12-07 21:02:20 +0100

    Add files via upload

---
+++ QA_Git.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class QA_Git
+{
+    public static void main(String[] args) throws Throwable
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://github.com/");
+        driver.findElement(By.id("user[login]")).sendKeys("s12313134");
+        driver.findElement(By.id("user[email]")).sendKeys("s12313134grgr@gmail.com");
+        driver.findElement(By.id("user[password]")).sendKeys("s12313134");
+        driver.findElement(By.xpath("//*[contains(text(), 'Sign up for GitHub')]")).click();
+
+
+    }
+}
