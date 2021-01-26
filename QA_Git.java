0680d29a1b6dcce29f082f8075c8ecf6bcfaf26e
Nikola <37514211+nikola011381@users.noreply.github.com>
2021-01-26 20:22:31 +0100

    Update QA_Git.java

--- QA_Git.java
+++ QA_Git.java
@@ -10,9 +10,9 @@ public class QA_Git
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://github.com/");
-        driver.findElement(By.id("user[login]")).sendKeys("s12313134");
-        driver.findElement(By.id("user[email]")).sendKeys("s12313134grgr@s12313134.com");
-        driver.findElement(By.id("user[password]")).sendKeys("s12313134");
+        driver.findElement(By.id("user[login]")).sendKeys("test");
+        driver.findElement(By.id("user[email]")).sendKeys("test@test.com");
+        driver.findElement(By.id("user[password]")).sendKeys("test");
         driver.findElement(By.xpath("//*[contains(text(), 'Sign up for GitHub')]")).click();
 
 
